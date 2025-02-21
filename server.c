71fc84819adcbbdd5a0899104c84406bc647ad6e
Ayse Feyza Serbest <aserbest@k2m04s02.42kocaeli.com.tr>
2025-02-21 13:08:56 +0300

    global variable düzenlendi

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: aserbest <aserbest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 07:43:03 by aserbest          #+#    #+#             */
-/*   Updated: 2025/02/20 20:03:25 by aserbest         ###   ########.fr       */
+/*   Updated: 2025/02/21 13:08:22 by aserbest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,36 +18,32 @@
 
 static int g_server = 0;
 
-static void	decode_data(int signal)
+static void	handle_message(int signal)
 {
 	static char	c = 0;
 	static char	i = 0;
 
-	c <<= 1;
+	c = (c << 1) & ~1;
 	if (signal == SIGUSR1)
-		c |= 1;
+    	c |= 1;
 	i++;
 	if (i == 8)
 	{
-		ft_printf("%c");
+		write(1, &c, 1);
 		i = 0;
 		if (c == '\0')
-		{
 			g_server = 1;
-			return;
-		}
-		c = 0;
 	}
 }
 
 static void	handle_signal(int signal, siginfo_t *info, void *context)
 {
 	(void)context;
-	decode_data(signal);
-	if (g_server)
+	handle_message(signal);
+	if (g_server == 1)
 	{
 		kill(info->si_pid, SIGUSR2);
-		ft_printf("\n");
+		write(1, "\n", 1);
 		g_server = 0;
 	}
 	else
@@ -56,14 +52,14 @@ static void	handle_signal(int signal, siginfo_t *info, void *context)
 
 int	main(void)
 {
-	struct sigaction	siginfo;
+	struct sigaction	sa;
 
-	ft_printf("\n\nSERVER PID: %d\n\n", getpid());
-	siginfo.sa_flags = SA_SIGINFO;
-	sigemptyset(&siginfo.sa_mask);
-	siginfo.sa_sigaction = handle_signal;
-	sigaction(SIGUSR1, &siginfo, NULL);
-	sigaction(SIGUSR2, &siginfo, NULL);
+	ft_printf("\nSERVER PID: %d\n", getpid());
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = handle_signal;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
 		pause();
 	return (0);
